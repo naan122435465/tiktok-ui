57be5d495bead3c11ed98afd4a66edbf46e419c8
nana122435465 <lenak45a@gmail.com>
2022-06-24 16:50:23 +0700

    header

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,10 @@
+import classNames from 'classnames/bind';
+
+import style from './Siderbar.module.scss';
+
+const cx = classNames.bind(style);
 function SideBar() {
-    return <h2>SideBar</h2>;
+    return <aside className={cx('wrapper')}>SideBar</aside>;
 }
 
 export default SideBar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,17 @@
-import Header from './Header';
+import className from 'classnames/bind';
+
+import styles from './DefaultLayoutmodule.scss';
+import Header from '~/components/Layout/components/Header';
 import SideBar from './SideBar';
+
+const cx = className.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <SideBar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
